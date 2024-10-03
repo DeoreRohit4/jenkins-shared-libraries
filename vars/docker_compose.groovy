5e77887821eed1dea255039b681ea688832667aa
Rohit Deore <102886808+DeoreRohit4@users.noreply.github.com>
2024-10-03 18:00:48 +0530

    Update docker_compose.groovy

--- vars/docker_compose.groovy
+++ vars/docker_compose.groovy
@@ -1,6 +1,6 @@
 def call(){
         echo "Code is deploying"
-        sh "docker compose up -d"
-        // sh "docker-compose down && docker-compose up -d"
+        //sh "docker compose up -d"
+        sh "docker-compose down && docker-compose up -d"
         echo "Code Deployed"
 }
